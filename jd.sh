73500bebe010ef795fe975450d73f96104c138fa
舞动的风 <615563901@qq.com>
2021-02-12 21:48:40 +0800

    update jd.sh.

--- jd.sh
+++ jd.sh
@@ -3,7 +3,7 @@
 ## Author: Evine Deng
 ## Source: https://github.com/EvineDeng/jd-base
 ## Modified： 2021-02-04
-## Version： v3.9.0
+## Version： v4.0.0
 
 ## 路径
 ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
