3b722b7734c91fcec14854136a142f98fbbd6087
wudongdefeng <615563901@qq.com>
2021-03-19 12:53:33 +0800

    update

--- sample/diy.sh
+++ sample/diy.sh
@@ -1,37 +1,9 @@
 #!/usr/bin/env bash
-
-## 文件路径、脚本网址、文件版本以及各种环境的判断
 ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
 [[ ${JD_DIR} ]] && ShellJd=jd || ShellJd=${ShellDir}/jd.sh
 LogDir=${ShellDir}/log
 [ ! -d ${LogDir} ] && mkdir -p ${LogDir}
 ScriptsDir=${ShellDir}/scripts3
-ConfigDir=${ShellDir}/config
-FileConf=${ConfigDir}/config.sh
-FileDiy=${ConfigDir}/diy.sh
-FileConfSample=${ShellDir}/sample/config.sh.sample
-ListCron=${ConfigDir}/crontab.list
-ListCronLxk=${ScriptsDir}/docker/crontab_list.sh
-ListTask=${LogDir}/task.list
-ListJs=${LogDir}/js.list
-ListJsAdd=${LogDir}/js-add.list
-ListJsDrop=${LogDir}/js-drop.list
-ContentVersion=${ShellDir}/version
-ContentNewTask=${ShellDir}/new_task
-ContentDropTask=${ShellDir}/drop_task
-SendCount=${ShellDir}/send_count
-isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ScriptsURL=https://github.com.cnpmjs.org/i-chenzhe/qx
-ShellURL=https://github.com/wudongdefeng/jd-base
-
-
-## 克隆scripts3
-function Git_CloneScripts {
-  git clone -b main ${ScriptsURL} ${ScriptsDir}
-  ExitStatusScripts=$?
-  echo
-}
-
 ## 更新scripts3
 function Git_PullScripts {
   cd ${ScriptsDir}
