49ff7b5a1bb280fa9f9af0683803036f9c03667b
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-03-31 20:40:55 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -21,8 +21,8 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ScriptsURL=https://github.com/wudongdefeng/jd_scripts
-ShellURL=https://gitee.com/wudongdefeng/jd-base
+ScriptsURL=https://gitee.com/wudongdefeng/jd_scripts
+ShellURL=https://github.com/wudongdefeng/jd-base
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
 ## 每天只更新一次,(分.时.延迟)为随机cron
