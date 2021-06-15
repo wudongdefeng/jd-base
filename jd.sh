d15251614fb3562072fecd008e1714ae15387644
wudongdefeng <615563901@qq.com>
2021-06-15 20:52:19 +0800

    update

--- jd.sh
+++ jd.sh
@@ -100,7 +100,7 @@ function Combin_All {
   export DDFACTORY_SHARECODES=$(Combin_Sub ForOtherJdFactory "T019_qwtFEtHolbeIRv3lP8CjVWnYaS5kRrbA@T0225KkcR09Lo1TVIhullfVedwCjVWnYaS5kRrbA@T0205KkcJ2x4nB6lQ0aH76FwCjVWnYaS5kRrbA")
   export JDZZ_SHARECODES=$(Combin_Sub ForOtherJdzz "S_qwtFEtHolbeIRv3lP8@S5KkcR09Lo1TVIhullfVedw@S5KkcJ2x4nB6lQ0aH76Fw")
   export JDJOY_SHARECODES=$(Combin_Sub ForOtherJoy "wWFOrC7_0Js_PzOOsCjhnw==@x0FKqaNRojnvTZGPkGhEIqt9zd5YaBeE@BWpGQBc1unxqf_2Xe-XV1A==")
-  export JXNC_SHARECODES=$(Combin_Sub ForOtherJxnc '{"smp":"da6762718787c913e5285470772b3b09","active":"jdnc_1_3yuanshupian210601_2","joinnum":2}'@'{"smp":"da6762718787c913e5285470772b3b09","active":"jdnc_1_3yuanshupian210601_2","joinnum":2}')
+  export JXNC_SHARECODES=$(Combin_Sub ForOtherJxnc '{"smp":"da6762718787c913e5285470772b3b09","active":"jdnc_1_3yuanshupian210601_2","joinnum":2}'@'{"smp":"da6762718787c913e5285470772b3b09","active":"jdnc_1_3yuanshupian210601_2","joinnum":2}'@'{"smp":"1e51775af56a39a17086a73469e7b093","active":"jdnc_1_3yuanganju210601_2","joinnum":1}')
   export JDHEALTH_SHARECODES=$(Combin_Sub ForOtherHealth "T019_qwtFEtHolbeIRv3lP8CjVfnoaW5kRrbA@T0225KkcR09Lo1TVIhullfVedwCjVfnoaW5kRrbA")
   export BOOKSHOP_SHARECODES=$(Combin_Sub ForOtherBookShop "05d692a781834dc6815b90d440059d09@93c846eef3e8439ab0a655bc4c9b21e1@3113559ee4aa44469645507954dae9b4")
   export JD818_SHARECODES=$(Combin_Sub ForOther818PHONE "ac4aacb4-2a43-459e-91a9-5941ed941bd0@d2884286-98b3-4aea-a4b7-bf0cd31830f3@58141890-c54a-4b52-b246-e0e123a98a11")
